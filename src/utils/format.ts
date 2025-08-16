2ae2c8217159a5cd8163e91398c5328d3c23ec4c
enigmaapos <apostoliaenigma@gmail.com>
2025-08-16 12:30:32 +0800

    Update format.ts

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,7 +1,3 @@
-// File: src/utils/format.ts
-// Description: Utility function for formatting Ethereum addresses.
-// ------------------------------------
-export function formatAddress(address: `0x${string}` | undefined): string {
-  if (!address) return '';
-  return `${address.substring(0, 6)}...${address.substring(address.length - 4)}`;
+export const shortenAddress = (address: string) => {
+  return `${address.slice(0, 6)}...${address.slice(-4)}`
 }
