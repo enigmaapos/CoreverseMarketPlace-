2b04eb4e18cb71e1c4ada8dacd2d565df258f372
enigmaapos <apostoliaenigma@gmail.com>
2025-08-16 12:25:30 +0800

    Create page.tsx

---
+++ src/app/nft/page.tsx
@@ -0,0 +1,8 @@
+export default function NFTPage() {
+  return (
+    <div className="py-10">
+      <h1 className="text-3xl font-bold">NFT Marketplace</h1>
+      <p>Browse all NFTs listed on Coreverse.</p>
+    </div>
+  )
+}
