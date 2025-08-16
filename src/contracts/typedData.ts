4af437d9d70245ccaa371834627ef1df0a9658a9
enigmaapos <apostoliaenigma@gmail.com>
2025-08-16 12:31:40 +0800

    Update typedData.ts

--- src/contracts/typedData.ts
+++ src/contracts/typedData.ts
@@ -1,16 +1,6 @@
-// File: src/contracts/typedData.ts
-// Description: Defines the EIP-712 typed data structure for the `Listing` object.
-// ------------------------------------
-export const listingTypes = {
-  Listing: [
-    { name: 'maker', type: 'address' },
-    { name: 'nft', type: 'address' },
-    { name: 'tokenId', type: 'uint256' },
-    { name: 'amount', type: 'uint256' },
-    { name: 'currency', type: 'address' },
-    { name: 'price', type: 'uint256' },
-    { name: 'startTime', type: 'uint256' },
-    { name: 'endTime', type: 'uint256' },
-    { name: 'nonce', type: 'uint256' },
-  ],
-} as const;
+export const EIP712Domain = [
+  { name: 'name', type: 'string' },
+  { name: 'version', type: 'string' },
+  { name: 'chainId', type: 'uint256' },
+  { name: 'verifyingContract', type: 'address' },
+]
